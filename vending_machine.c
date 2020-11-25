981e921ec9ed560e40a5891cce9d9f599bf0776e
Radiantly <44368997+radiantly@users.noreply.github.com>
2020-11-25 22:52:46 +0530

    Add stepper motor logic

--- vending_machine.c
+++ vending_machine.c
@@ -82,42 +82,83 @@ void LCD_CHAR (char msg)
 		IO0CLR = 0x00000040; /* EN = 0, RS and RW unchanged(i.e. RS = 1, RW = 0) */
 		delay_ms(5);
 }
+
+void printBeverage(char * name, char * price) {
+	LCD_CMD(0x01);
+	LCD_STRING(name);
+	LCD_CMD(0xC0);
+	LCD_STRING(price);
+}
+
 char val_j[3];
 int main(void)
 {	
 	LCD_INIT();
-	PINSEL1 = 0x0000000C; /* Configure P0.1 as EINT0 */	
-	IO1DIR = 0x00000100; /* P0.8 as output to LED */
 	LCD_STRING("Choose your");
 	LCD_CMD(0xC0);
 	LCD_STRING("beverage:");
+	PINSEL1 = 0x00000000;
 	PINSEL2 = 0x00000000;
-	IO1DIR = 0x00000000;
-	IO0DIR |= 0x000F0000;
+	IO0DIR |= 0xFFFF0000;
+	IO1DIR |= 0x00010000;
 	while(1) {
 		if ((IO1PIN & (1 << 20)) == 0) {
-			LCD_CMD(0x01);
-			LCD_STRING("Tea");
-			LCD_CMD(0xC0);
-			LCD_STRING("Rs 10");
+			printBeverage("Tea", "Rs 10");
+			IO0PIN = ( (IO0PIN | 0x00010000) & 0xFFF1FFFF );
+			delay_ms(100);
+			
+			IO0PIN = ( (IO0PIN | 0x00020000) & 0xFFF2FFFF );
+			delay_ms(100);
+			
+			IO0PIN = ( (IO0PIN | 0x00040000) & 0xFFF4FFFF );
+			delay_ms(100);
+			
+			IO0PIN = ( (IO0PIN | 0x00080000) & 0xFFF8FFFF );
+			delay_ms(100);
 		}
 		if ((IO1PIN & (1 << 22)) == 0) {
-			LCD_CMD(0x01);
-			LCD_STRING("Coffee");
-			LCD_CMD(0xC0);
-			LCD_STRING("Rs 20");
+			printBeverage("Coffee", "Rs 20");
+			IO0PIN = ( (IO0PIN | 0x00100000) & 0xFF1FFFFF );
+			delay_ms(100);
+			
+			IO0PIN = ( (IO0PIN | 0x00200000) & 0xFF2FFFFF );
+			delay_ms(100);
+			
+			IO0PIN = ( (IO0PIN | 0x00400000) & 0xFF4FFFFF );
+			delay_ms(100);
+			
+			IO0PIN = ( (IO0PIN | 0x00800000) & 0xFF8FFFFF );
+			delay_ms(100);
 		}
 		if ((IO1PIN & (1 << 24)) == 0) {
-			LCD_CMD(0x01);
-			LCD_STRING("Coca Cola");
-			LCD_CMD(0xC0);
-			LCD_STRING("Rs 35");
+			printBeverage("Coca Cola", "Rs 35");
+			IO0PIN = ( (IO0PIN | 0x02000000) & 0xE3FFFFFF );
+			delay_ms(100);
+			
+			IO0PIN = ( (IO0PIN | 0x04000000) & 0xE5FFFFFF );
+			delay_ms(100);
+			
+			IO0PIN = ( (IO0PIN | 0x08000000) & 0xE9FFFFFF );
+			delay_ms(100);
+			
+			IO0PIN = ( (IO0PIN | 0x10000000) & 0x11FFFFFF );
+			delay_ms(100);
 		}
 		if ((IO1PIN & (1 << 26)) == 0) {
-			LCD_CMD(0x01);
-			LCD_STRING("Lime Juice");
-			LCD_CMD(0xC0);
-			LCD_STRING("Rs 15");
+			printBeverage("Lime Juice", "Rs 15");
+			IO1PIN &= 0xFFFEFFFF;
+			IO0PIN = ( (IO0PIN | 0x20000000) & 0x3FFFFFFF );
+			delay_ms(100);
+			
+			IO0PIN = ( (IO0PIN | 0x40000000) & 0x5FFFFFFF );
+			delay_ms(100);
+			
+			IO0PIN = ( (IO0PIN | 0x80000000) & 0x9FFFFFFF );
+			delay_ms(100);
+			
+			IO1PIN |= 0x10000;
+			IO0PIN &= 0x1FFFFFFF;
+			delay_ms(100);
 		}
 	}
 }
